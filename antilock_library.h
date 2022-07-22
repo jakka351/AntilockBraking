13b228141b92c63b66b8e67f20f707b08c9af7a4
jakka351 <57064943+jakka351@users.noreply.github.com>
2022-07-22 13:32:48 +0000

    Update antilock_library.h

--- antilock_library.h
+++ antilock_library.h
@@ -1,3 +1,48 @@
+#!/usr/bin/python3
+#coding:utf-8
+#################################################################################################################
+#'''__    ___________    ____  /\     ___________             _________                                   __   
+#  / /    \_   _____/___/_   | \ \    \_   _____/ ____  __ __ \_   ___ \  ____   _____   _____   ______   \ \  
+# / /      |    __)/ ___\|   |  \ \    |    __)__/ ___\|  |  \/    \  \/ /  _ \ /     \ /     \ /  ___/    \ \ 
+# \ \      |     \/ /_/  >   |   \ \   |        \  \___|  |  /\     \___(  <_> )  Y Y  \  Y Y  \\___ \     / / 
+#  \_\     \___  /\___  /|___|    \ \ /_______  /\___  >____/  \______  /\____/|__|_|  /__|_|  /____  >   /_/  
+#              \//_____/           \/         \/     \/               \/             \/      \/     \/         
+#                                                                       Falcon Diagnostic Utility - [FG1ECUComms]
+#################################################################################################################
+# FG1ECUComms.py
+# https://github.com/jakka351/FG-Falcon
+#################################################################################################################
+# NOTICE:
+# Copyright (c) 2022 
+# bjakkaleighton@gmail.com // jack.leighton@au.bosch.com
+# All rights reserved.
+#################################################################################################################
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+# the following conditions are met:
+# 1.    With the express written consent of the copyright holder.
+#
+# 2.    Redistributions of source code must retain the above copyright notice, this 
+#       list of conditions and the following disclaimer.
+# 3.    Redistributions in binary form must reproduce the above copyright notice, this 
+#       list of conditions and the following disclaimer in the documentation and/or other 
+#       materials provided with the distribution.
+# 4.    Neither the name of the organization nor the names of its contributors may be used to
+#       endorse or promote products derived from this software without specific prior written permission.
+#################################################################################################################
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#################################################################################################################
+# Notes.
+#  This software can only be distributed with my written permission. It is for my own educational purposes and 
+#     is potentially dangerous to ECU health and safety. 
+#################################################################################################################
+
+
 //#################################################################################################################
 //# Antilock Brake Module     
 //#################################################################################################################
